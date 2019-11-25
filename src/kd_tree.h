409e0dfa1841c4e00b9071b3f821d5fc2c82cb7b
Ken Kawamoto <ken.kawamoto@kittyhawk.aero>
2019-11-25 10:24:40 -0800

    convert recursion to loop

--- src/kd_tree.h
+++ src/kd_tree.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 // Structure to represent node of kd tree
 template <typename PointT>
 struct Node
@@ -77,39 +79,6 @@ inline bool is_nearby(const pcl::PointXYZ& a, const pcl::PointXYZ& b, float tole
     return dx <= tolerance and dy <= tolerance and dz <= tolerance;
 }
 
-template<typename PointT>
-static void check_at_depth(const Node<PointT>& current,
-                           int depth,
-                           const PointT& target,
-                           float tolerance,
-                           std::vector<int>& neighbor) {
-    int idx_to_compare = depth % Dimension(current.point);
-
-    if (std::abs(Value(current.point, idx_to_compare) - Value(target, idx_to_compare)) <= tolerance) {
-        // if `current` is close to `target` at least in one dimension,
-        // - run strict check and add `current` to `neighbor` if truly in neighbor
-        // - check both childen recursively.
-
-        if (is_nearby(current.point, target, tolerance)) {
-            neighbor.push_back(current.id);
-        }
-
-        if (current.left != nullptr) {
-            check_at_depth(*current.left, depth + 1, target, tolerance, neighbor);
-        }
-        if (current.right != nullptr) {
-            check_at_depth(*current.right, depth + 1, target, tolerance, neighbor);
-        }
-    } else {
-        // if `current` is not close enough to `target` in the dimension, check only one child.
-        if (Value(target, idx_to_compare) < Value(current.point, idx_to_compare) and current.left != nullptr) {
-            check_at_depth(*current.left, depth + 1, target, tolerance, neighbor);
-        } else if (current.right != nullptr) {
-            check_at_depth(*current.right, depth + 1, target, tolerance, neighbor);
-        }
-    }
-}
-
 
 template<typename PointT>
 class KdTree {
@@ -134,13 +103,50 @@ class KdTree {
 
     /// return a list of point ids in the tree that are within distance of target.
     std::vector<int> search(const PointT& target, float distanceTol) const {
-        std::vector<int> ids;
+        std::vector<int> neighbor;
 
-        if (root != nullptr) {
-            check_at_depth(*root, 0, target, distanceTol, ids);
+        if (root == nullptr) {
+            return neighbor;
         }
 
-        return ids;
+        std::queue<std::pair<const Node<PointT>*, int>> queue;
+        queue.push(std::make_pair<const Node<PointT>*, int>(root, 0));
+        size_t dim = Dimension(root->point);
+
+        while (not queue.empty()) {
+            auto pair = queue.front();
+            queue.pop();
+
+            const Node<PointT>* node = std::get<0>(pair);
+            int depth = std::get<1>(pair);
+            int idx_to_compare = depth % dim;
+
+            if (std::abs(Value(node->point, idx_to_compare) - Value(target, idx_to_compare)) <= distanceTol) {
+                // if `node` is close to `target` at least in one dimension,
+                // - run strict check and add `node` to `neighbor` if truly in neighbor
+                // - check both childen recursively.
+
+                if (is_nearby(node->point, target, distanceTol)) {
+                    neighbor.push_back(node->id);
+                }
+
+                if (node->left != nullptr) {
+                    queue.push(std::make_pair(node->left, depth + 1));
+                }
+                if (node->right != nullptr) {
+                    queue.push(std::make_pair(node->right, depth + 1));
+                }
+            } else {
+                // if `node` is not close enough to `target` in the dimension, check only one child.
+                if (Value(target, idx_to_compare) < Value(node->point, idx_to_compare) and node->left != nullptr) {
+                    queue.push(std::make_pair(node->left, depth + 1));
+                } else if (node->right != nullptr) {
+                    queue.push(std::make_pair(node->right, depth + 1));
+                }
+            }
+        }
+
+        return neighbor;
     }
 
   private:
